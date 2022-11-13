328c11c74b8fa3d8f14d451f96e166afa462600a
Aaron <ken888686@gmail.com>
2022-11-13 22:25:42 +0900

    move setState to onChange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,29 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: "" };
 
   render() {
     return (
       <div className="ui segment">
         <form action="submit" className="ui form">
           <div className="field">
-            <label htmlFor="search">Image Search</label>
+            <label htmlFor="term">Image Search</label>
             <input
               type="text"
-              name="search"
-              id="search"
+              name="term"
+              id="term"
+              value={this.state.term}
               // 不加括號 -> 觸發執行
               // 加括號 -> render 的時候就會執行
-              onChange={this.onInputChange}
+              onChange={(e) =>
+                this.setState({ term: e.target.value.toUpperCase() })
+              }
             />
           </div>
+          <button type="submit" className="ui primary button">
+            Submit
+          </button>
         </form>
       </div>
     );
