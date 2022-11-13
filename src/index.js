cb9ae4424a8e2e25563584b4555149765b65ff0d
Aaron <ken888686@gmail.com>
2022-11-13 19:52:59 +0900

    update lifecycle

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 class App extends React.Component {
+  /*
+  do one time setup
+  state initialization
+  API request
+  */
   constructor(props) {
     super(props);
-
     this.state = { lat: null, lng: null, errorMessage: "" };
+  }
 
+  /*
+  do something only one time
+  data loading
+  */
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({
@@ -16,13 +26,31 @@ class App extends React.Component {
         console.info(position.coords);
       },
       (err) => {
-        this.setState({
-          errorMessage: err.message,
-        });
+        this.setState({ errorMessage: err.message });
       }
     );
   }
 
+  /*
+  每次更新 state/props 都會被呼
+  */
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+    console.log(prevState);
+    console.log(this.state);
+  }
+
+  /*
+  移除 component 使用
+  */
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  /*
+  return JSX only
+  no network request
+  */
   render() {
     if (this.state.errorMessage && !this.state.lat && !this.state.lng) {
       return <div>Error: {this.state.errorMessage}</div>;
