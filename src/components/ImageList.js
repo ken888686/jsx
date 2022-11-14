effad686a04da40a7b205856e9f5a37a8f258902
Aaron <ken888686@gmail.com>
2022-11-14 21:54:16 +0900

    add Grid to beautify layout

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./ImageList.css";
 
 const ImageList = ({ images }) => {
   const imageList = images.map(({ id, urls, description }) => (
     <img key={id} src={urls.regular} alt={description} srcSet="" />
   ));
 
-  return imageList;
+  return <div className="image-list">{imageList}</div>;
 };
 
 export default ImageList;
