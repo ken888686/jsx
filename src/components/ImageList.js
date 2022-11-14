854517bb7847a2c5f7c952cb0a87af5b1f7faa32
Aaron <ken888686@gmail.com>
2022-11-14 22:06:25 +0900

    create ImageCard to customize image size

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageList = ({ images }) => {
-  const imageList = images.map(({ id, urls, description }) => (
-    <img key={id} src={urls.regular} alt={description} srcSet="" />
+  const imageList = images.map((image) => (
+    <ImageCard key={image.id} image={image} />
   ));
 
   return <div className="image-list">{imageList}</div>;
