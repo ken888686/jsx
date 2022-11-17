648ecf031da081c8f5c4b5a2ea04feae4d20d27b
Aaron <ken888686@gmail.com>
2022-11-17 23:23:04 +0900

    create onVideoSelect callback function

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,11 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderList = videos.map((video) => {
-    return <VideoItem video={video} key={video.etag} />;
+    return (
+      <VideoItem onVideoSelect={onVideoSelect} video={video} key={video.etag} />
+    );
   });
   return <div className="ui relaxed divided list">{renderList}</div>;
 };
