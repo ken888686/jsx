ce43be5f6debf205dd58eb3bdf1e3c800b1ad662
Aaron <ken888686@gmail.com>
2022-11-17 23:12:20 +0900

    create VideoItem css for custom style

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,11 @@
+import "./VideoItem.css";
 import React from "react";
 
 const VideoItem = ({ video }) => {
   const url = `https://www.youtube.com/watch?v=${video.id.videoId}`;
 
   return (
-    <div className="item">
+    <div className="video-item item">
       <img
         className="ui image"
         src={video.snippet.thumbnails.default.url}
