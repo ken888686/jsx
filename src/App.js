7f832961a28ce209e456ad9ecfa96fbdf60e9fee
Aaron <ken888686@gmail.com>
2022-11-19 23:59:56 +0900

    created Search component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from "react";
-import Accordion from "./components/Accordion";
-
-const items = [
-  {
-    title: "Title 1",
-    content: "Content 1",
-  },
-  {
-    title: "Title 2",
-    content: "Content 2",
-  },
-  {
-    title: "Title 3",
-    content: "Content 3",
-  },
-];
+import Search from "./components/Search";
 
 const App = () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
