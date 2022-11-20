be8ab8ec24b26822155811c77e0a534832c3bdfa
Aaron <ken888686@gmail.com>
2022-11-20 18:38:36 +0900

    add basic functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
-  return <h1>Search</h1>;
+  const [term, setTerm] = useState("");
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label htmlFor="term">Search</label>
+          <input
+            type="text"
+            name="term"
+            id="term"
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
