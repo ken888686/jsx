e23b26fc6bcebad785ee456f70bb563d4185db57
Aaron <ken888686@gmail.com>
2022-11-20 19:22:45 +0900

    put wiki search api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,25 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get(
+        `https://en.wikipedia.org/api/rest_v1/page/related/${term}`,
+        {
+          params: {},
+        }
+      );
+      setResults(data);
+    };
+
+    if (term) {
+      search();
+    }
+  }, [term]);
 
   return (
     <div>
