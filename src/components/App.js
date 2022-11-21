252fac9cd81e2da244f24cdf75e6d32909b03f49
Aaron <ken888686@gmail.com>
2022-11-21 23:37:47 +0900

    remove useless callback

--- src/components/App.js
+++ src/components/App.js
@@ -19,10 +19,6 @@ const App = () => {
     setSelectedVideo(response.data.items[0]);
   };
 
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video);
-  };
-
   return (
     <div className="ui container">
       <SearchBar onFormSubmit={onTermSubmit} />
@@ -32,7 +28,7 @@ const App = () => {
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
       </div>
