4f39e3de54b400cf2ac692bfd10f2c2b71a51587
Aaron <ken888686@gmail.com>
2022-11-21 23:07:21 +0900

    create Header and switch different pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
-const options = [
+const options_1 = [
   {
     label: "Red",
     value: "red",
@@ -17,10 +21,47 @@ const options = [
   },
 ];
 
+const options_2 = [
+  {
+    title: "Red",
+    content: "red",
+  },
+  {
+    title: "Green",
+    content: "green",
+  },
+  {
+    title: "Blue",
+    content: "blue",
+  },
+];
+
 const App = () => {
+  const onSelectedChange = () => {
+    console.log("onSelectedChange");
+  };
+
   return (
     <div>
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={options_2} />
+      </Route>
+      <Route path="/search">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          items={options_2}
+          label="Hi"
+          selected={options_1[0]}
+          onSelectedChange={onSelectedChange}
+          options={options_1}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate items={options_2} />
+      </Route>
     </div>
   );
 };
